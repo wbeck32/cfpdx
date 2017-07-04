d15f005db89d175f60d3f93471547b1699ea1db6
Wendy Beck <webeck@gmail.com>
2017-07-04 14:22:05 -0700

    removing a file

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-# rsync -rt  public/js/display.js wbeck@107.170.235.84:~/cfpdx/public/js/.
-# rsync -rt  public/js/maps.js wbeck@107.170.235.84:~/cfpdx/public/js/.
-# rsync -rt  public/index.html wbeck@107.170.235.84:~/cfpdx/public/.
-
-# rsync -rt  app.js wbeck@107.170.235.84:~/cfpdx/.
